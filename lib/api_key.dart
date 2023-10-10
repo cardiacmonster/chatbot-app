a4d4de782e5352a8cc0f350d87be5e8ef03a59e9
cardiacmonster <raj07092004@gmail.com>
2023-10-10 09:25:10 +0530

    first commit

---
+++ lib/api_key.dart
@@ -0,0 +1,3 @@
+class APIKey {
+  static const apiKey = "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";
+}
---
+++ lib/chatScreen.dart
@@ -0,0 +1,154 @@
+import 'dart:convert';
+
+import 'package:velocity_x/velocity_x.dart';
+
+import 'api_key.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+class ChatGPTScreen extends StatefulWidget {
+  @override
+  _ChatGPTScreenState createState() => _ChatGPTScreenState();
+}
+
+class _ChatGPTScreenState extends State<ChatGPTScreen> {
+  final List<Message> _messages = [];
+  late String len;
+
+
+  final TextEditingController _textEditingController = TextEditingController();
+
+  void onSendMessage() async {
+    Message message = Message(text: _textEditingController.text, isMe: true);
+
+    _textEditingController.clear();
+
+    setState(() {
+      _messages.insert(0, message);
+    });
+
+    String response = await sendMessageToChatGpt(message.text);
+
+    Message chatGpt = Message(text: response, isMe: false);
+
+    setState(() {
+      _messages.insert(0, chatGpt);
+    });
+    sendMessageToChatGpt(message.text);
+  }
+
+  Future<String> sendMessageToChatGpt(String message) async {
+    Uri uri = Uri.parse("https://api.openai.com/v1/chat/completions");
+
+    Map<String, dynamic> body = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        {"role": "user", "content": message}
+      ],
+      "max_tokens": 4000,
+    };
+
+
+    final response = await http.post(
+      uri,
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer ${APIKey.apiKey}",
+      },
+      body: json.encode(body),
+    );
+
+    print(response.body);
+
+    Map<String, dynamic> parsedReponse = json.decode(response.body);
+
+    String reply = parsedReponse['choices'][0]['message']['content'];
+
+    return reply;
+  }
+
+  Widget _buildMessage(Message message) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        child:Container(
+            decoration: BoxDecoration(
+
+              color: Colors.black12,
+              shape: BoxShape.rectangle,
+              border: Border.all(color: Colors.black,width:1),
+                borderRadius: BorderRadius.only(bottomRight: Radius.circular(1),topLeft: Radius.circular(15))
+
+            ),
+            child: Column(
+              crossAxisAlignment:
+              message.isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  message.isMe ? 'You' : 'GPT',
+                  style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black,fontSize: 18),
+                ),
+                Text(message.text,style: TextStyle(color: Colors.black,fontSize: 12),),
+              ],
+
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Persnol ChatBot'),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Column(
+
+          children: <Widget>[
+            Expanded(
+              child: ListView.builder(
+                reverse: true,
+                itemCount: _messages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return _buildMessage(_messages[index]);
+                },
+              ),
+            ),
+            Divider(height: 1.0),
+            Container(
+              decoration: BoxDecoration(color: Theme.of(context).cardColor),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      controller: _textEditingController,
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(10.0),
+                        hintText: 'Type a message...',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.send),
+                    onPressed: onSendMessage,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Message {
+  final String text;
+  final bool isMe;
+
+  Message({required this.text, required this.isMe});
+}
